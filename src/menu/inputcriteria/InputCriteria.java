302c9028d6ce4ea765ea4acf2020ad3802171c77
David Lu <davidlu1999@gmail.com>
2024-03-07 00:32:49 +0100

    Added InputCriteria

---
+++ src/menu/inputcriteria/InputCriteria.java
@@ -0,0 +1,43 @@
+package menu.inputcriteria;
+
+import ui.SystemMessages;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InputCriteria {
+    // Check if user input is an integer (doesn't work properly, have to press enter 2x after inputting a non-integer)
+    public int checkIfUserInputIsInt(int min, int max) {
+        boolean running = true;
+        Scanner scan = new Scanner(System.in);
+        int userChoice = 0;
+
+        while (running) {
+            try {
+                userChoice = scan.nextInt();
+                userChoice = checkIfIntIsBetweenMinAndMax(userChoice, min, max);
+                running = false;
+            } catch (InputMismatchException e) {
+                SystemMessages.printError("Input must be an integer\n");
+                SystemMessages.tryAgain();
+                scan.nextLine(); // Scanner bug
+            }
+        }
+        return userChoice;
+    }
+
+    // Check if integer is between a user-given min and max
+    private int checkIfIntIsBetweenMinAndMax(int userChoice, int min, int max) {
+        Scanner scan = new Scanner(System.in);
+        while (userChoice < min || userChoice > max) {
+            try {
+                SystemMessages.printError("Input must be between " + min + "-" + max + "\n");
+                SystemMessages.tryAgain();
+                userChoice = scan.nextInt();
+            } catch (InputMismatchException e) {
+                userChoice = checkIfUserInputIsInt(min, max);
+            }
+        }
+        return userChoice;
+    }
+}
