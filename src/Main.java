5fe06b64a71ff67d0c29085354050456a39db574
David Lu <davidlu1999@gmail.com>
2024-03-07 15:01:09 +0100

    Testing features

--- src/Main.java
+++ src/Main.java
@@ -57,21 +57,74 @@ public class Main {
         mySqlConnection.closeConnection();
     }
 
-    private void displayMenu() {
+    private void displayMainMenu() {
         System.out.println("\nCAR RENTAL SYSTEM MENU");
-        System.out.println("1. Add a car");
-        System.out.println("2. Delete a car");
-        System.out.println("3. Display cars");
+        System.out.println("1. Cars");
+        System.out.println("2. Customers");
+        System.out.println("3. Display");
         System.out.println("4. Quit");
     }
+
+    private void displayCarMenu() {
+        System.out.println("\nCAR MENU");
+        System.out.println("1. Add a car");
+        System.out.println("2. Update a car");
+        System.out.println("3. Delete a car");
+    }
+
+    private void carSubMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int carChoice;
+
+        do {
+            displayCarMenu();
+            System.out.print("Enter your choice (1-2): ");
+            carChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (carChoice) {
+                case 1:
+                    carModification.createCar();
+                    break;
+                case 2:
+                    carModification.updateCar();
+                case 3:
+                    deleteCar();
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 2.");
+            }
+
+        } while (carChoice != 2);
+    }
+
+    private void displayCustomerMenu() {
+        System.out.println("\nCUSTOMER MENU");
+        // kunde menu
+    }
+
+    private void customerSubMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int customerChoice;
+
+        do {
+            displayCustomerMenu();
+            System.out.print("Enter your choice (1-2): ");
+            customerChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            // kunde relations
+
+        } while (customerChoice != 2);
+    }
+
 
     private void deleteCar() {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter car ID to delete: ");
         int carId = in.nextInt();
 
-        carModification.deleteCar(carId);
-        System.out.println("Car deleted successfully.");
+        carModification.deleteCarById(carId);
     }
 
 }
