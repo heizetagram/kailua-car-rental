3a0dace3a9fe679ddc0ec5e7baaba2af3d04548c
David Lu <davidlu1999@gmail.com>
2024-03-08 15:41:12 +0100

    Moved methods

--- src/ui/SystemMessages.java
+++ src/ui/SystemMessages.java
@@ -9,19 +9,27 @@ public class SystemMessages {
         System.out.printf("%s%s%s", ConsoleColors.GREEN_BRIGHT, text, ConsoleColors.RESET);
     }
 
+    public static void printError(String text) {
+        System.out.printf("%s%s%s", ConsoleColors.RED, text, ConsoleColors.RESET);
+    }
+
+    public static void printTryAgain() {
+        printError("Please try again...\n");
+    }
+
     public static void printQuitProgram() {
-        SystemMessages.printBrightRed("Exiting the program. Goodbye!");
+        SystemMessages.printBrightRedText("Exiting the program. Goodbye!");
     }
 
     public static void printQuitOption(int n) {
-        printBrightRed(n + ". Quit\n");
+        printBrightRedText(n + ". Quit\n");
     }
 
-    public static void printGreenBright(String text) {
+    public static void printGreenBrightText(String text) {
         System.out.printf("%s%s%s", ConsoleColors.GREEN_BRIGHT, text, ConsoleColors.RESET);
     }
 
-    public static void printGreen(String text) {
+    public static void printGreenText(String text) {
         System.out.printf("%s%s%s", ConsoleColors.GREEN, text, ConsoleColors.RESET);
     }
 
@@ -31,14 +39,6 @@ public class SystemMessages {
 
     public static void printYellowText(String text) {
         System.out.printf("%s%s%s", ConsoleColors.YELLOW, text, ConsoleColors.RESET);
-    }
-
-    public static void printError(String text) {
-        System.out.printf("%s%s%s", ConsoleColors.RED, text, ConsoleColors.RESET);
-    }
-
-    public static void tryAgain() {
-        printError("Please try again...\n");
     }
 
     public static void printBoldBlueText(String text) {
@@ -73,7 +73,7 @@ public class SystemMessages {
         System.out.printf("%s%s%s", ConsoleColors.CYAN_BOLD, text, ConsoleColors.RESET);
     }
 
-    public static void printBrightRed(String text) {
+    public static void printBrightRedText(String text) {
         System.out.printf("%s%s%s", ConsoleColors.RED_BRIGHT, text, ConsoleColors.RESET);
     }
 }
