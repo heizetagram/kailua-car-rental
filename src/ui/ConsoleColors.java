82ced109cbb72c5290ffb5e4772a10e13f680459
David Lu <davidlu1999@gmail.com>
2024-03-08 03:09:09 +0100

    Added more colors

--- src/ui/ConsoleColors.java
+++ src/ui/ConsoleColors.java
@@ -1,17 +1,28 @@
 package ui;
 
-    public class ConsoleColors {
-        public static final String RESET = "\u001b[0m";
-        public static final String RED = "\u001b[0;31m";
-        public static final String GREEN = "\u001b[0;32m";
-        public static final String YELLOW = "\u001b[0;33m";
-        public static final String BLUE = "\u001b[0;34m";
-        public static final String RED_BOLD = "\u001b[1;31m";
-        public static final String YELLOW_BOLD = "\u001b[1;33m";
-        public static final String BLUE_BOLD = "\u001b[1;34m";
-        public static final String BLACK_BRIGHT = "\u001b[0;90m";
-        public static final String GREEN_BRIGHT = "\u001b[0;92m";
-        public static final String GREEN_BOLD_BRIGHT = "\u001b[1;92m";
-        public ConsoleColors() {
-        }
-    }
+public class ConsoleColors {
+    // Reset
+    public static final String RESET = "\033[0m";  // Text Reset
+
+    // Regular Colors
+    public static final String RED = "\033[0;31m";     // RED
+    public static final String GREEN = "\033[0;32m";   // GREEN
+    public static final String YELLOW = "\033[0;33m";  // YELLOW
+    public static final String BLUE = "\033[0;34m";    // BLUE
+    public static final String CYAN = "\033[0;36m";    // CYAN
+
+    // Bold
+    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+    public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
+    public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
+
+    // High Intensity
+    public static final String RED_BRIGHT = "\033[0;91m";    // RED
+    public static final String GREEN_BRIGHT = "\033[0;92m";  // GREEN
+    public static final String PURPLE_BRIGHT = "\033[0;95m"; // PURPLE
+    public static final String CYAN_BRIGHT = "\033[0;96m";   // CYAN
+
+    // Bold High Intensity
+    public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";// PURPLE
+    public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";  // CYAN
+}
