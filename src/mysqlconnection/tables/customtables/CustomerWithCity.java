9effac29de765426cb9887a2a659eb6af167457a
David Lu <davidlu1999@gmail.com>
2024-03-08 15:41:33 +0100

    Changed to subclass

--- src/mysqlconnection/tables/customtables/CustomerWithCity.java
+++ src/mysqlconnection/tables/customtables/CustomerWithCity.java
@@ -1,75 +1,18 @@
 package mysqlconnection.tables.customtables;
 
+import mysqlconnection.tables.Customer;
+
 import java.time.LocalDate;
 
-public class CustomerWithCity {
-    private int customerId;
-    private String firstName;
-    private String lastName;
-    private String address;
-    private int postalCode;
+public class CustomerWithCity extends Customer  {
     private String city;
-    private String mobilePhone;
-    private String phoneNumber;
-    private String email;
-    private String licenseNumber;
-    private LocalDate issueDate;
 
     public CustomerWithCity(int customerId, String firstName, String lastName, String address, int postalCode, String city, String mobilePhone, String phoneNumber, String email, String licenseNumber, LocalDate issueDate) {
-        this.customerId = customerId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.postalCode = postalCode;
+        super(customerId, firstName, lastName, address, postalCode, mobilePhone, phoneNumber, email, licenseNumber, issueDate);
         this.city = city;
-        this.mobilePhone = mobilePhone;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.licenseNumber = licenseNumber;
-        this.issueDate = issueDate;
-    }
-
-    public int getCustomerId() {
-        return customerId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public int getPostalCode() {
-        return postalCode;
     }
 
     public String getCity() {
         return city;
-    }
-
-    public String getMobilePhone() {
-        return mobilePhone;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getLicenseNumber() {
-        return licenseNumber;
-    }
-
-    public LocalDate getIssueDate() {
-        return issueDate;
     }
 }
--- src/mysqlconnection/tables/customtables/RentalWithFullInfo.java
+++ src/mysqlconnection/tables/customtables/RentalWithFullInfo.java
@@ -1,15 +1,10 @@
 package mysqlconnection.tables.customtables;
 
+import mysqlconnection.tables.Rental;
+
 import java.time.LocalDateTime;
 
-public class RentalWithFullInfo {
-    private int rentalId;
-    private int customerId;
-    private int carId;
-    private LocalDateTime fromDate;
-    private LocalDateTime toDate;
-    private int maxKm;
-    private int currentKm;
+public class RentalWithFullInfo extends Rental {
     private String firstName;
     private String lastName;
     private String registrationNumber;
@@ -17,46 +12,12 @@ public class RentalWithFullInfo {
     private String model;
 
     public RentalWithFullInfo(int rentalId, int customerId, int carId, LocalDateTime fromDate, LocalDateTime toDate, int maxKm, int currentKm, String firstName, String lastName, String registrationNumber, String brand, String model) {
-        this.rentalId = rentalId;
-        this.customerId = customerId;
-        this.carId = carId;
-        this.fromDate = fromDate;
-        this.toDate = toDate;
-        this.maxKm = maxKm;
-        this.currentKm = currentKm;
+        super(rentalId, customerId, carId, fromDate, toDate, maxKm, currentKm);
         this.firstName = firstName;
         this.lastName = lastName;
         this.registrationNumber = registrationNumber;
         this.brand = brand;
         this.model = model;
-    }
-
-    public int getRentalId() {
-        return rentalId;
-    }
-
-    public int getCustomerId() {
-        return customerId;
-    }
-
-    public int getCarId() {
-        return carId;
-    }
-
-    public LocalDateTime getFromDate() {
-        return fromDate;
-    }
-
-    public LocalDateTime getToDate() {
-        return toDate;
-    }
-
-    public int getMaxKm() {
-        return maxKm;
-    }
-
-    public int getCurrentKm() {
-        return currentKm;
     }
 
     public String getFirstName() {
